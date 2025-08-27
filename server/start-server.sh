4f998568f4beec761ab4b2b9f2500737c964bd0c
matheus <matheuscf6@gmail.com>
2025-08-27 08:47:16 -0300

    projeto finalizado

--- server/start-server.sh
+++ server/start-server.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 # Usa ipconfig do Windows diretamente, ignora 127.x.x.x e extrai o primeiro IP válido
-IP=$(/c/Windows/System32/ipconfig.exe | grep -i "IPv4" | grep -v "127.0.0.1" | sed 's/.*: //' | head -n 1)
-
-# Verifica se IP foi obtido
-if [ -z "$IP" ]; then
-  echo "Erro ao obter o IP da máquina."
-  exit 1
-fi
 
 # Verifica se db.json existe
 if [ ! -f db.json ]; then
@@ -16,5 +9,5 @@ if [ ! -f db.json ]; then
 fi
 
 # Inicia o json-server
-echo "Iniciando json-server em http://$IP:3000 ..."
-json-server -H 127.0.0.1 -p 3000 db.json
+
+json-server -H 192.168.40.64 -p 9000 db.json
